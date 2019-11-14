fae68b245d7a80925f64fbd3702b8142882b183e
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-11-14 10:27:22 -0300

    list products in cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
 import { Container, ProductTable, Total } from './styles';
+import { connect } from 'react-redux';
 
-export default function Cart() {
+function Cart({ cart }) {
   return (
     <Container>
       <ProductTable>
@@ -16,37 +17,41 @@ export default function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <img
-                src='https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x'
-                alt='image'
-              />
-            </td>
-            <td>
-              <strong> Tenis</strong>
-              <span>R${99.00}</span>
-            </td>
-            <td>
-              <div>
-              <button type='button'>
-                <MdRemoveCircleOutline size={20} color='#7159c1' />
-              </button>
-              <input type='number' readOnly value={1} />
-              <button type='button'>
-                <MdAddCircleOutline size={20} color='#7159c1' />
-              </button>
-              </div>
-            </td>
-            <td>
-              <strong>R$ {200.00}</strong>
-            </td>
-            <td>
-              <button type='button'>
-                <MdDelete size='20' color='#7159c1' />
-              </button>
-            </td>
-          </tr>
+          {
+            cart.map(product => (
+              <tr>
+                <td>
+                  <img
+                    src={product.image}
+                    alt={product.title}
+                  />
+                </td>
+                <td>
+                  <strong> {product.title}</strong>
+                  <span>{product.priceFormated}</span>
+                </td>
+                <td>
+                  <div>
+                    <button type='button'>
+                      <MdRemoveCircleOutline size={20} color='#7159c1' />
+                    </button>
+                    <input type='number' readOnly value={product.amount} />
+                    <button type='button'>
+                      <MdAddCircleOutline size={20} color='#7159c1' />
+                    </button>
+                  </div>
+                </td>
+                <td>
+                  <strong>R$ {200.00}</strong>
+                </td>
+                <td>
+                  <button type='button'>
+                    <MdDelete size='20' color='#7159c1' />
+                  </button>
+                </td>
+              </tr>
+            ))
+          }
         </tbody>
       </ProductTable>
       <footer>
@@ -59,3 +64,7 @@ export default function Cart() {
     </Container>
   );
 }
+const stateToProps = state => ({
+  cart: state.cart
+})
+export default connect(stateToProps)(Cart);
