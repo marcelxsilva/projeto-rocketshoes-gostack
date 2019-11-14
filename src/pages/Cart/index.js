e5e306ff9888586c932a484f065c369f5fe70726
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-11-14 10:46:37 -0300

    remove product from cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -3,7 +3,7 @@ import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons
 import { Container, ProductTable, Total } from './styles';
 import { connect } from 'react-redux';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -45,7 +45,7 @@ function Cart({ cart }) {
                   <strong>R$ {200.00}</strong>
                 </td>
                 <td>
-                  <button type='button'>
+                  <button type='button' onClick={() => dispatch({ type: 'REMOVE_PRODUCT_CART', id: product.id })}>
                     <MdDelete size='20' color='#7159c1' />
                   </button>
                 </td>
